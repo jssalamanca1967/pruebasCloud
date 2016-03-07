6940b4f65bd77038dba3b83137937a271f5c8abc
jssalamanca1967 <js.salamanca1967@uniandes.edu.co>
2016-03-06 16:49:27 -0800

    Avanzado hasta apps-views-disenio

--- DesignMatch/app/controllers/application_controller.rb
+++ DesignMatch/app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_empresa
+  def current_empresa
+    @current_empresa ||= Empresa.find(session[:empresa_id]) if session[:empresa_id]
+  end
+
+  def require_empresa
+    redirect_to '/login' unless current_empresa
+  end
 end
